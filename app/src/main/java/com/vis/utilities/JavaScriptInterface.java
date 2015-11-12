8220cfa32c61e36f9981d2a25a84eb548b262255
huzefagadi <huzefar52@gmail.com>
2015-11-12 16:25:10 +0530

    -facebookShare

--- app/src/main/java/com/vis/utilities/JavaScriptInterface.java
+++ app/src/main/java/com/vis/utilities/JavaScriptInterface.java
@@ -1,14 +1,13 @@
 package com.vis.utilities;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.webkit.JavascriptInterface;
-import android.widget.Toast;
 
 import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.ShareVideo;
+import com.facebook.share.model.ShareVideoContent;
 import com.facebook.share.widget.ShareDialog;
 
 /**
@@ -17,19 +16,31 @@ import com.facebook.share.widget.ShareDialog;
 public class JavaScriptInterface {
     private Activity activity;
     ShareDialog shareDialog;
+
     public JavaScriptInterface(Activity activiy) {
         this.activity = activiy;
     }
-    @JavascriptInterface
-    public void shareOnFacebook(String videoAddress){
-        shareDialog = new ShareDialog(activity);
-        if (ShareDialog.canShow(ShareLinkContent.class)) {
-            ShareLinkContent linkContent = new ShareLinkContent.Builder()
-                    .setContentTitle("Video In Short")
-                    .setContentUrl(Uri.parse(videoAddress))
-                    .build();
 
-            shareDialog.show(linkContent);
+    @JavascriptInterface
+    public void shareOnFacebook(String videoAddress) {
+        shareDialog = new ShareDialog(activity);
+        String youtubetag = "youtube.com/watch?v=";
+        String id = videoAddress.substring(videoAddress.indexOf(youtubetag) + youtubetag.length(), videoAddress.length());
+        if (ShareDialog.canShow(ShareLinkContent.class)) {
+
+            ShareVideoContent videoContent = new ShareVideoContent.Builder()
+                    .setContentUrl(Uri.parse(videoAddress))
+
+                    .setContentTitle("test")
+                    .setPreviewPhoto(new SharePhoto.Builder().setImageUrl(Uri.parse("http://img.youtube.com/vi/" + id + "/0.jpg")).build())
+                    .build();
+            /*ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                    .setContentTitle("Video in Short")
+                    .setImageUrl(Uri.parse("http://img.youtube.com/vi/" + id + "/0.jpg"))
+                    .setContentUrl(Uri.parse(videoAddress))
+                    .build();*/
+
+            shareDialog.show(videoContent);
         }
 
     }
